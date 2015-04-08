464bf658c4c7160466092ea8d2518f0672ae6df0
Eric Sullivan <e_sullivan9@post.massbay.edu>
2015-04-08 19:43:40 -0400

    v47894359843691

---
+++ Tables.sql
@@ -0,0 +1,5 @@
+create table Users(
+Username varchar(12) NOT NULL,
+Password varchar(20) NOT NULL,
+Section char(20) NOT NULL,
+PRIMARY KEY(Username));
---
+++ src/Backend/StartUpManager.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Backend;
+
+import Gui.*;
+import java.sql.*;
+
+/**
+ * This class controls the start up of the medical doctor program. It detects if this is the first time start up, in which cause
+ * the program opens the newAdmin frame, or it start the program into the normal login.
+ * 
+ * 
+ * Database Name: honorsmedicaldoctor
+ * 
+ * @author Eric Sullivan
+ */
+public class StartUpManager {
+
+    public static void main(String[] args){
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    Class.forName("com.mysql.jdbc.Driver");
+                    Connection con = DriverManager.getConnection("jdbc:mysql://localhost/honorsmedicaldoctor", "root", "CSCI400"); //change password for it to work.
+                    String sql = "select * from Users where Section = 'admin'";
+                    Statement stmt = con.createStatement();
+                    ResultSet rs = stmt.executeQuery(sql);
+                    
+                    if(rs.next()){
+                        System.out.println("There is One");
+                        
+                        //new Login().setVisible(true);
+                    }
+                    
+                    else 
+                        new NewAdmin().setVisible(true);
+                    con.close();
+                    rs.close();
+                }
+                catch(Exception ex){
+                    ex.printStackTrace();
+                }
+                
+            }
+        });
+    }
+}
