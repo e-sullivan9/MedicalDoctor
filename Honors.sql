1e20d567a0372fa95d29bca52b32d1b189ee9a5a
Samir <Samir@10.10.188.229>
2015-04-16 17:48:00 -0400

    Honors.sql

--- Honors.sql
+++ Honors.sql
@@ -1,4 +1,4 @@
- /*
+/*
 DROP DATABASE honorsmedicaldoctor;
 DROP USER 'HonorsAdmin'@'localhost';
 */
@@ -26,3 +26,36 @@ create table Patients (SSN varchar(11) NOT NULL,
 					Gender char(20) NOT NULL,
 					NextVisit date NOT NULL,
 					PRIMARY KEY(SSN));
+
+create table Visits(
+VID int AUTO_INCREMENT,
+SSN varchar(11) NOT NULL,
+VisitDate date NOT NULL,
+ChiefComplaint char(100) NOT NULL,
+PresentIllness char(100) NOT NULL,
+PastHistory char(100) NOT NULL,
+ReviewOfTheSystem char(100) NOT NULL,
+PhysicalExam char(100) NOT NULL,
+Impression char(100) NOT NULL,
+DiagnosisByDoctor char(100) NOT NULL,
+NursingActivity char(100) NOT NULL,
+PRIMARY KEY(VID),
+FOREIGN KEY(SSN) REFERENCES Patients(SSN) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+create table Labs (
+LabID int AUTO_INCREMENT,
+VID int NOT NULL,
+RedBloodCell boolean NOT NULL,
+WhiteBloodCell boolean NOT NULL,
+LiverFunction boolean NOT NULL,
+RenalFunction boolean NOT NULL,
+Electrolyte boolean NOT NULL,
+XRay boolean NOT NULL,
+ComputedTomography boolean NOT NULL,
+MRI boolean NOT NULL,
+UrinaryTest boolean NOT NULL,
+StoolTest boolean NOT NULL,
+PRIMARY KEY(LabID),
+FOREIGN KEY(VID) REFERENCES Visits(VID) ON UPDATE CASCADE ON DELETE CASCADE
+);
