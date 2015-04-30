daf9b0b829361b529fac842305db266de56efb2f
samirleal <samirtleal@yahoo.com>
2015-04-29 20:44:51 -0400

    Header added to the top of every file using License Header Plugin.

--- src/medicaldoctor/NewJFrame.java
+++ src/medicaldoctor/NewJFrame.java
@@ -1,9 +1,19 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Team Name: Andrew's Honor Squad 2: High Honors
+ * Team Members: Justin Jiang, Samir Leal, Phi Nguyen, Eric Sullivan
+ * Date: 04-30-15
+ *
+ * Purpose: You and your team members will simulate to develop the software called
+ * "Medical Doctor".  The software will be used in hospital for managing and updating
+ * not only the patient information but also prescription, and other physical test.
+ * The software should be developed with well-designed Graphical User Interface (GUI)
+ * so that medical doctors can easily access and manage the patient information.
+ *
+ * Program Contents: The "Medical Doctor" software should have following contents on GUI:
+ * 1. Registration
+ * 2. Doctor General Practice
+ * 3. Nursing
  */
-
 package medicaldoctor;
 
 /**
